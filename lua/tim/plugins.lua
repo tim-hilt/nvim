2ac1b0be0aaa480e95a788a71384298ce2002bf9
Tim Hilt <thilt@in-technology.eu>
2023-01-25 13:54:13 +0100

    main: Scripted auto-commit on change (2023-01-25 13:54:13) by gitwatch.sh

--- lua/tim/plugins.lua
+++ lua/tim/plugins.lua
@@ -105,16 +105,16 @@ return require("packer").startup(function()
 	-- 		require("github-theme").setup({})
 	-- 	end,
 	-- })
-	-- use({
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	after = "github-nvim-theme",
-	-- 	requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	-- 	config = function()
-	-- 		require("lualine").setup({
-	-- 			options = { theme = "auto", globalstatus = true },
-	-- 		})
-	-- 	end,
-	-- })
+	use({
+		"nvim-lualine/lualine.nvim",
+		after = "gruvbox.",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup({
+				options = { theme = "auto", globalstatus = true },
+			})
+		end,
+	})
 	use({
 		"nvim-telescope/telescope.nvim",
 		config = require("tim.telescope-config").config,
