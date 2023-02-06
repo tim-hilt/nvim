5ba0ad613fdde25b398ffc6ed1e1686e20e15cf8
Tim Hilt <thilt@in-technology.eu>
2023-02-06 10:26:00 +0100

    main: Scripted auto-commit on change (2023-02-06 10:26:00) by gitwatch.sh

--- lua/tim/plugins.lua
+++ lua/tim/plugins.lua
@@ -249,6 +249,8 @@ return require("packer").startup(function()
 	use({
 		"folke/which-key.nvim",
 		config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
 			require("which-key").setup({})
 		end,
 	})
