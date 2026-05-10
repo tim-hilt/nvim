00b4ee2c80477accd31c9b6de41064ff93759279
Tim Hilt <timhilt@live.de>
2026-05-10 23:17:43 +0200

    switch to pack

---
+++ init.lua
@@ -0,0 +1,31 @@
+vim.pack.add({
+	"https://github.com/smoka7/hop.nvim",
+	"https://github.com/max397574/better-escape.nvim",
+	"https://github.com/HiPhish/rainbow-delimiters.nvim"
+})
+
+local hop = require("hop")
+hop.setup({ keys = "enaistcr,h/.vdqj" })
+vim.keymap.set("n", "s", hop.hint_char2)
+
+require("better_escape").setup {
+    default_mappings = false,
+    mappings = {
+        i = {
+            m = {
+                n = "<Esc>"
+            },
+        },
+    },
+}
+
+require("rainbow-delimiters.setup").setup()
+
+vim.keymap.set("n", "'", function() vim.cmd(":wa") end)
+
+vim.wo.number = true
+
+-- TODO
+-- * copy to unamedplus
+-- * Highlight current line
+
