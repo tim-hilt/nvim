dc5cb09da2c8b7beb68a00d00362be91c0fbc1be
Tim Hilt <timhilt@live.de>
2026-07-09 20:16:09 +0200

    restore cursor position

--- init.lua
+++ init.lua
@@ -28,3 +28,12 @@ vim.wo.number = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
+
